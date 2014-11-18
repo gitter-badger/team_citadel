0e51722730328f458621fe265436d88cdb84fcf2
Christopher Paterson <cjpaterson.3@gmail.com>
2014-11-18 15:14:27 +0000

    Add code ready for use to ListingController

--- app/controllers/ListingController.php
+++ app/controllers/ListingController.php
@@ -2,85 +2,129 @@
 
 class ListingController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		// get all of the currently live listings
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        // get all of the currently live listings
+    }
 
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		// show form to add a new listing
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        // show form to add a new listing
+        // return View::make(route, compact(stuff-required-for-this-view));
+    }
 
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		// save the new listing
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        // save the new listing
+
+        // $listingData = Input::only(
+        //    // names of the form elements we want to use in here
+        // );
+
+        // check it conforms to our requirements
+        // $validator = Validator::make($listingData, [
+        //     // validation rules go here
+        // ]);
+
+        // if ($validator->fails()) {
+        //     // what do we return if it fails to add?
+        // }
+        // else {
+        //     $listing = Listing::create($listingData);
+
+        //     return $listing;
+        // }
+    }
 
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		// show an individual listing
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        // show an individual listing
+
+        // $listing = Listing::find($id);
+    }
 
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		// show form to edit a listing
-	}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        // show form to edit a listing
+        // return View::make(route, compact(stuff-required-for-this-view));
+
+    }
 
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		// save changes to the listing
-	}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+    //  // save changes to the listing
+        // $listing = Listing::find($id);
+
+        // $listingData = Input::only(
+        //     // names of the form elements we want to use in here
+        // );
+
+        // $validator = Validator::make($listingData, [
+        //     // validation rules go here
+        // ]);
+
+        // if ($validator->fails()) {
+        //     // what do we do if it fails?
+        // }
+        // else {
+        //     // update the listing
+        //     $listing->update($listingData);
+        //     return $listing;
+        // }
+    }
 
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		// delete the listing
-	}
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        // // delete the listing
+        // $Listing = Listing::destroy($id);
+
+        // return $id;
+    }
 
 
 }
