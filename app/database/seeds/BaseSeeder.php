09a5dde547abdbb79340c155b6aa63f71d233b1c
Aled Davies <aleddavies123@yahoo.co.uk>
2014-11-18 17:59:15 +0000

    create a csv function to seed database

---
+++ app/database/seeds/BaseSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+class BaseSeeder extends seeder
+{
+    protected $table;
+    protected $filename;
+
+    //run this to see the database
+    public function run()
+    {
+        DB::table($this->table)->truncate();
+        $seedData = $this->seedFromCSV($this->filename, ',');
+        DB::table($this->table)->insert($seedData);
+    }
+
+    //method for processing data from the csv file
+    public function seedFromCSV($fileName, $deliminator = ",")
+    {
+        //check file exists and is readable
+        if (!file_exists($filename) || !is_readable($filename)) {
+            return false;
+        }
+
+        $header = null;
+        $data = [];
+
+        
+        if (($handle = fopen($filename, 'r')) !== false) {
+            while (($row = fgetcsv($handle, 1000, $deliminator)) !== false) {
+                if (!$header) {
+                    $header = $row;
+                } else {
+                    $data[] = array_combine($header, $row);
+                }
+            }
+            fclose($handle);
+        }
+        return $data;
+    }
+}
