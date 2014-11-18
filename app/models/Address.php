9d5e2eb0451ff002ac77cab467883d748674a950
Aled Davies <aleddavies123@yahoo.co.uk>
2014-11-18 14:47:36 +0000

    Luke's review corrections

--- app/models/Address.php
+++ app/models/Address.php
@@ -5,7 +5,7 @@ use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent
+class Address extends Eloquent
 {
 
     protected $table = 'address';
