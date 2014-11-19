dcf1a3c35255b14a99ce9a380857bc3e8d432518
Aled Davies <aleddavies123@yahoo.co.uk>
2014-11-19 11:38:52 +0000

    DB:seed card information completed

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder {
     {
         Eloquent::unguard();
 
-        $this->call('SeedCards');
+        $this->call('GameSeeder');
+        $this->call('SeriesSeeder');
+        $this->call('CardSeeder');
     }
 }
