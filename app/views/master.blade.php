7f291fe066671550b48f65a2f186550e70d8faeb
Aled Davies <aleddavies123@yahoo.co.uk>
2014-11-24 16:59:20 +0000

    create decks table and a pivot table

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -82,12 +82,11 @@
             </div>
         </div>
     </div>
+    @section('scripts')
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    @show
 </body>
-
-@section('scripts')
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="{{asset('js/bootstrap.min.js')}}"></script>
-@stop
 </html>
--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -1,59 +1,68 @@
 @extends( 'master' )
-@section('header')
-    <h2>Profile {{{ $user->username }}}</h2>
-@stop
+    @section('header')
+        <h2>Profile {{{ $user->username }}}</h2>
+    @stop
 
-@section('content')
-    <div class="col-md-12">
-        <div class="col-md-2">
-            <img class="responsive-image center-block" src="/images/users/{{$user->id}}.jpeg" width="70%">
-        </div>
+    @section('content')
+        <div class="col-md-12">
+            <div class="col-sm-4 col-md-3">
+                <img class="responsive-image center-block" src="/images/users/{{$user->id}}.jpeg" width="70%">
+            </div>
 
-        <div class="col-md-10">
-            <ul class="nav nav-tabs nav-justified">
-                <li class="active"><a href="#profile-tab" data-toggle="tab">Profile<i class="fa"></i></a></li>
-                <li><a href="#wall-tab" data-toggle="tab">Wall<i class="fa"></i></a></li>
-            </ul>
+            <div class="col-sm-8 col-md-9 ">
+                <ul class="nav nav-tabs nav-pill">
 
-            <div class="tab-content">
-                <div class="tab-pane active" id="profile-tab">
-                    <h3> {{ $user->username }} </h3>
-                    <table class="table">
-                        <tr>
-                            <td width=10%>Title</td>
-                            <td>{{ $user->title }}</td>
-                        </tr>
-                        <tr>
-                            <td>Firstname</td>
-                            <td>{{ $user->first_name }}</td>
-                        </tr>
-                        <tr>
-                            <td>Lastname</td>
-                            <td>{{ $user->last_name}}
-                        </tr>
-                        <tr>
-                            <td>Email</td>
-                            <td>{{$user->email_address}}
-                        </tr>
-                        <tr>
-                            <td>DOB</td>
-                            <td>{{$user->date_of_birth}}
-                        </tr>
-                        <tr>
-                            <td>Location</td>
-                            <td>{{$user->location}}</td>
-                        </tr>
-                        <tr>
-                            <td>Mobile Number</td>
-                            <td>{{$user->mobile_phone}}
-                        </tr>
-                        <tr>
-                            <td>Home Number</td>
-                            <td>{{$user->home_telephone}}
-                        </tr>
-                    </table>
+                    <li><a href="#profile-tab2" data-toggle="tab" >Profile<i class="fa"></i></a></li>
+                    <li><a href="#wall-tab" data-toggle="tab" >Wall<i class="fa"></i></a></li>
+                </ul>
+
+                <div class="tab-content">
+                    <div class="tab-pane" id="profile-tab2">
+                        <h3> {{{ $user->username }}} </h3>
+                        <table class="table">
+                            <tr>
+                                <td width=10%>Title</td>
+                                <td>{{{ $user->title }}}</td>
+                            </tr>
+                            <tr>
+                                <td>Firstname</td>
+                                <td>{{{ $user->first_name }}}</td>
+                            </tr>
+                            <tr>
+                                <td>Lastname</td>
+                                <td>{{{ $user->last_name }}} </td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td>{{{ $user->email_address }}}</td>
+                            </tr>
+                            <tr>
+                                <td>DOB</td>
+                                <td>{{{ $user->date_of_birth }}}</td>
+                            </tr>
+                            <tr>
+                                <td>Location</td>
+                                <td>{{{ $user->location }}}</td>
+                            </tr>
+                            <tr>
+                                <td>Mobile Number</td>
+                                <td>{{{ $user->mobile_phone }}}</td>
+                            </tr>
+                            <tr>
+                                <td>Home Number</td>
+                                <td>{{{ $user->home_telephone }}}</td>
+                            </tr>
+                        </table>
+                    </div>
+
+                    <div class="tab-pane" id="wall-tab">
+                        <p>hello</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-@stop
+    @stop
+
+    @section('scripts')
+        @parent
+    @stop
