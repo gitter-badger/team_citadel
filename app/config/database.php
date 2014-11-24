59ea3355bf29cf525c82a5e69e9cd5e688d9db93
Christopher Paterson <cjpaterson.3@gmail.com>
2014-11-24 12:24:53 +0000

    further DB changes

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'team_citadel',
+			'database'  => 'ebdb',
 			'username'  => 'teamcitadel',
 			'password'  => 'SuperChar',
 			'charset'   => 'utf8',
