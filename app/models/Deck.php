518442bb8d0438756a2484e3b510da5dcaa2a961
Aled Davies <aleddavies123@yahoo.co.uk>
2014-11-26 18:11:16 +0000

    Decks Model

---
+++ app/models/Deck.php
@@ -0,0 +1,17 @@
+<?php
+
+class Deck extends Eloquent
+{
+    protected $table = 'decks';
+    protected $guard = 'id';
+
+    public function users()
+    {
+        return $this->belongsToMany('User');
+    }
+
+    public function cards()
+    {
+        return $this->belongsToMany('Card');
+    }
+}
