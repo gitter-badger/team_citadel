1edb0934e78c6801b16afe181fff3af6caa9a0b2
Christopher Paterson <cjpaterson.3@gmail.com>
2014-11-28 11:29:12 +0000

    added disabled fields for things we dont have

--- app/views/series.blade.php
+++ app/views/series.blade.php
@@ -5,19 +5,19 @@
 @section('content')
     <div class='row'>
         @foreach($series as $aSeries)
-            <div class='col-xs-12 col-sm-2'>
-                    <div style="min-height:140px">
-                        @if(count($aSeries->cards->all()) > 0) 
-                            <a class="series-box" href="{{ $aSeries->url }}">
-                                <img class="series-images image-responsive center-block" src="{{ asset('images/series/'. $aSeries->id . '.jpg') }}" width="90%">
-                            </a>
-                        @else
-                            <img class="series-images-alt image-responsive center-block" src="{{ asset('images/series/'. $aSeries->id . '.jpg') }}" width="90%">
-                        @endif
-                    </div>
-                    <h5 class='text-center series-card-name' title="{{{ $aSeries->name }}}">
-                            {{ $aSeries->name }}
-                    </h5>
+            @if(count($aSeries->cards->all()) > 0) 
+                <div class='col-xs-12 col-sm-2'>
+            @else
+                <div class='col-xs-12 col-sm-2 disabled'>
+            @endif
+                <div style="min-height:140px">
+                        <a class="series-box" href="{{ $aSeries->url }}">
+                            <img class="series-images image-responsive center-block" src="{{ asset('images/series/'. $aSeries->id . '.jpg') }}" width="90%">
+                        </a>
+                </div>
+                <h5 class='text-center series-card-name' title="{{{ $aSeries->name }}}">
+                        {{ $aSeries->name }}
+                </h5>
             </div>
         @endforeach
     </div>
