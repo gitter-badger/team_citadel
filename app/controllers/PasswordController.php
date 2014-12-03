6d79d86c685286e48d3db8bef79c62e702e78d52
Adam Jama <adamjama7@hotmail.co.uk>
2014-12-03 12:30:55 +0000

    username reminder implemented

--- app/controllers/PasswordController.php
+++ app/controllers/PasswordController.php
@@ -44,12 +44,14 @@ class PasswordController extends BaseController {
 		$result = Password::remind( Input::only('email'), function($message) {
     		$message->subject('Password Reminder');
         	$message->from('noreply@deckcitadel.com', 'Deck Citadel');
-   		});
+   		}, 'emails.auth.usernamereminder');
 
 		
 		if ($result == PasswordBroker::REMINDER_SENT) {
 		// TODO: take the user to homepage with email sent message
 			return Redirect::route('welcome')->with('message', 'Username reminder sent!');
+		} else if ($result == PasswordBroker::INVALID_USER){
+			return Redirect::route('welcome')->with('error', 'Invalid user!');
 		} else {
 			return Redirect::route('welcome')->with('error', 'Username reminder not sent!');
 		}
@@ -57,19 +59,22 @@ class PasswordController extends BaseController {
 	
 	public function usernameupdate()
 	{
-		$cred = array('email' => Input::get('email'),
-		'username' => Input::get('username'), 
-		'username_confirmation' => Input::get('username_confirmation'),
+		$credentials = array('email' => Input::get('email'),
+		// We need to trick password reset mechanism for username 
+		'password' => Input::get('username'), 
+		'password_confirmation' => Input::get('username_confirmation'),
 		'token' => Input::get('token'));
 		
-		$result = Password::reset($cred, function($user, $username)
+		$result = Password::reset($credentials, function($user, $username)
 			{
-				$user->password = Hash::make($password);
+				$user->username = ($username);
 				$user->save();
 			});
 
 		if (PasswordBroker::PASSWORD_RESET == $result) { // i.e. Success
-			return Redirect::to('login')->with('message', 'Your password has been reset, please log in!');
+			return Redirect::to('login')->with('message', 'Your username has been reset, please log in!');
+		} else if (PasswordBroker::INVALID_PASSWORD == $result) {
+			return Redirect::to('username/reset/{token}')->withInput()->with('error', 'Your username do not match!');
 		} else if (PasswordBroker::INVALID_USER == $result) {
 			return Redirect::to('username/reset/{token}')->withInput()->with('error', 'Invalid user!');
 		} else if (PasswordBroker::INVALID_TOKEN == $result) {
