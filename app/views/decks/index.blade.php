0502d977c92230d0038f46e7bdbd575d442ec77d
Christopher Paterson <cjpaterson.3@gmail.com>
2014-12-11 11:56:42 +0000

    fixed bug where a deck with no cards would break the /decks page as it could not find first card to get the image of.

--- app/views/decks/index.blade.php
+++ app/views/decks/index.blade.php
@@ -8,34 +8,36 @@
 
 @section('content')
      @foreach($decks as $deck)
-        <div class="row">
-            <div class="col-md-12  post-border">
-                <span class="col-sm-3 col-md-2 vCenter ">
-                    <div class="img_wrapper_cards">
-                        <img class="responsive-image center-block" src="{{ $deck->cards->first()->getMediumImageURL() }}" width="70%" onload="imgLoaded(this)">
-                    </div>
-                </span>
-                <div class="col-md-9 vCenter">
-                    <div class=''>
-                        <h4>{{{ ucwords($deck->title) }}}</h4> {{ link_to_route('deck.show', 'See full deck', [$deck->id]) }}
-                    </div>
-                    <hr>
-                    <div class="">
-                        <p>{{{ $deck->description }}}</p>
-                    </div>
-                    <hr>
-                     <div class="row">
-                        <div class="col-md-6">
-                            Created: {{ $deck->user->created_at->diffForHumans()}} by {{ link_to_route('user.show', $deck->user->username, [$deck->user->username]) }}
+        @if(count($deck->cards) > 0)
+            <div class="row">
+                <div class="col-md-12  post-border">
+                    <span class="col-sm-3 col-md-2 vCenter ">
+                        <div class="img_wrapper_cards">
+                            <img class="responsive-image center-block" src="{{ $deck->cards->first()->getMediumImageURL() }}" width="70%" onload="imgLoaded(this)">
                         </div>
+                    </span>
+                    <div class="col-md-9 vCenter">
+                        <div class=''>
+                            <h4>{{{ ucwords($deck->title) }}}</h4> {{ link_to_route('deck.show', 'See full deck', [$deck->id]) }}
+                        </div>
+                        <hr>
+                        <div class="">
+                            <p>{{{ $deck->description }}}</p>
+                        </div>
+                        <hr>
+                         <div class="row">
+                            <div class="col-md-6">
+                                Created: {{ $deck->user->created_at->diffForHumans()}} by {{ link_to_route('user.show', $deck->user->username, [$deck->user->username]) }}
+                            </div>
 
-                        <div class="col-md-4 pull-right">
+                            <div class="col-md-4 pull-right">
 
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endif
     @endforeach
     <div class="row text-center">
         {{ $decks->links() }}
