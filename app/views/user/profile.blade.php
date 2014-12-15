31f3b4e102bd81d1778963758c7723baa96d875e
Christopher Paterson <cjpaterson.3@gmail.com>
2014-12-15 14:29:42 +0000

    add check to see if there are messages for the user before trying to show them

--- app/views/user/profile.blade.php
+++ app/views/user/profile.blade.php
@@ -59,23 +59,25 @@
 
             <div class="tab-content col-md-10">
                 <div class="tab-pane active" id="tab_b">
-                @foreach($messages as $message)
-                <div class="panel panel-info">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Inbox</h3>
-                    </div>
-                    <div class="panel-body">
-                        <p>
-                @foreach($message->conversation->users as $user)
-                    {{{ $user->username }}}
-                @endforeach
-                        </p>
-                        <p>{{{ $message->conversation->name }}}</p>
-                        <p>{{{ $message->content }}}</p>
-                        <a href="{{{ URL::route('reply.message', $user->username) }}}" class="btn btn-info">Reply</a>
-                    </div>
-                </div>
-                @endforeach
+                @if(isset($messages))
+                    @foreach($messages as $message)
+                        <div class="panel panel-info">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Inbox</h3>
+                            </div>
+                            <div class="panel-body">
+                                <p>
+                        @foreach($message->conversation->users as $user)
+                            {{{ $user->username }}}
+                        @endforeach
+                                </p>
+                                <p>{{{ $message->conversation->name }}}</p>
+                                <p>{{{ $message->content }}}</p>
+                                <a href="{{{ URL::route('reply.message', $user->username) }}}" class="btn btn-info">Reply</a>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
             </div>
 
             <div class="tab-pane" id="tab_c">
