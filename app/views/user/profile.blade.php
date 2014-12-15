1e1ad970c1086043957c6c4cfd5828616e0b20ea
Christopher Paterson <cjpaterson.3@gmail.com>
2014-12-15 15:07:18 +0000

    layout changes as well as redeleting unused files

--- app/views/user/profile.blade.php
+++ app/views/user/profile.blade.php
@@ -1,30 +1,30 @@
 @extends('master')
 @section('content')
-<!-- Nav tabs -->
-<ul class="nav nav-tabs col-md-12" role="tablist">
-    <li role="presentation" class="active"><a href="#wall-tab" role="tab" data-toggle="tab">Wall</a></li>
-    @if(Auth::check())
-        @if(Auth::user()->id == $user->id)
-            <li role="presentation"><a href="#profile" role="tab" data-toggle="tab">Profile</a></li>
-            <li role="presentation"><a href="#messages" role="tab" data-toggle="tab">Messages</a></li>
-            <li role="presentation"><a href="#settings" role="tab" data-toggle="tab">Settings</a></li>
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs col-md-12" role="tablist">
+        <li role="presentation" class="active"><a href="#wall-tab" role="tab" data-toggle="tab">Wall</a></li>
+        @if(Auth::check())
+            @if(Auth::user()->id == $user->id)
+                <li role="presentation"><a href="#profile" role="tab" data-toggle="tab">Profile</a></li>
+                <li role="presentation"><a href="#messages" role="tab" data-toggle="tab">Messages</a></li>
+                <li role="presentation"><a href="#settings" role="tab" data-toggle="tab">Settings</a></li>
+            @endif
         @endif
-    @endif
-</ul>
-<!-- Tab panes -->
-<div class="tab-content col-md-12">
-    <div class="tab-pane active" id="wall-tab">
-        @if(Auth::check() && Auth::user()->id == $user->id)
-            <div class="row well post-border">
-                <div class="col-md-3">
-                    <h4 class="text-center">
-                        <a href="/decks/create">
-                            Create Deck <icon class="glyphicon glyphicon-plus-sign"></icon>
-                        </a>
-                    </h4>
+    </ul>
+    <!-- Tab panes -->
+    <div class="tab-content col-md-12">
+        <div class="tab-pane active" id="wall-tab">
+            @if(Auth::check() && Auth::user()->id == $user->id)
+                <div class="row well post-border">
+                    <div class="col-md-3">
+                        <h4 class="text-center">
+                            <a href="/decks/create">
+                                Create Deck <icon class="glyphicon glyphicon-plus-sign"></icon>
+                            </a>
+                        </h4>
+                    </div>
                 </div>
-            </div>
-        @endif
+            @endif
             @foreach($decks as $deck)
                 <div class="row">
                     <div class="col-md-12 post-border">
@@ -38,7 +38,7 @@
                     </div>
                 </div>
             @endforeach
-    </div>
+        </div>
 
         <div role="tabpanel" class="tab-pane" id="messages">
             <div class="row well post-border">
@@ -102,52 +102,52 @@
             </div>
         </div>
 
-            <div role="tabpanel" class="tab-pane" id="profile">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="page-header">
-                            <h1>Your Profile</h1>
-                        </div>
-                    </div>
-                </div>
+        <div role="tabpanel" class="tab-pane" id="profile">
+            <div class="row">
                 <div class="col-md-12">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <img class="img-circle profile-img" src="{{ asset(  'images/users/' . $user->id . '.jpeg') }}">
-                        </div>
-                        <div class="col-md-6">
-                            <h3> {{{ $user->username }}} </h3>
-                            <table class="table table-hover">
-                                <tr>
-                                    <td>Title</td>
-                                    <td>{{{ $user->title }}}</td>
-                                </tr>
-                                <tr>
-                                    <td>Firstname</td>
-                                    <td>{{{ $user->first_name }}}</td>
-                                </tr>
-                                <tr>
-                                    <td>Lastname</td>
-                                    <td>{{{ $user->last_name }}}
-                                </tr>
-                                <tr>
-                                    <td>Email</td>
-                                    <td>{{{ $user->email }}}
-                                </tr>
-                                <tr>
-                                    <td>DOB</td>
-                                    <td>{{{ $user->date_of_birth }}}
-                                </tr>
-                                <tr>
-                                    <td>Location</td>
-                                    <td>{{{ $user->location }}}</td>
-                                </tr>
-                            </table>
-                            <a href="{{ route('edit.user', $user->username) }}" class="btn btn-custom btn-lg btn-block"> Edit</a>
-                        </div>
+                    <div class="page-header">
+                        <h1>Your Profile</h1>
                     </div>
                 </div>
             </div>
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img class="img-circle profile-img" src="{{ asset(  'images/users/' . $user->id . '.jpeg') }}" width="50%">
+                    </div>
+                    <div class="col-md-6">
+                        <h3> {{{ $user->username }}} </h3>
+                        <table class="table table-hover">
+                            <tr>
+                                <td>Title</td>
+                                <td>{{{ $user->title }}}</td>
+                            </tr>
+                            <tr>
+                                <td>Firstname</td>
+                                <td>{{{ $user->first_name }}}</td>
+                            </tr>
+                            <tr>
+                                <td>Lastname</td>
+                                <td>{{{ $user->last_name }}}
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td>{{{ $user->email }}}
+                            </tr>
+                            <tr>
+                                <td>DOB</td>
+                                <td>{{{ $user->date_of_birth }}}
+                            </tr>
+                            <tr>
+                                <td>Location</td>
+                                <td>{{{ $user->location }}}</td>
+                            </tr>
+                        </table>
+                        <a href="{{ route('edit.user', $user->username) }}" class="btn btn-primary btn-lg btn-block"> Edit</a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
         <div role="tabpanel" class="tab-pane" id="messages">Future Updates</div>
--- app/views/widgets/item-unavailable.blade.php
+++
@@ -1,7 +0,0 @@
-<h3>{{ $card->name }} - {{ $card->series->name . $card->serial_number }}</h3>
-<table class="table table-striped">
-    <tr>
-        <td><strong>Price</strong></td>
-        <td>Available to order</td>
-    </tr>
-</table>
